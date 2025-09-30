d5a1c28e37d8f04f24e5b77c130e239fa7a3e128
Zailio Labs <100409086+zailio-labs@users.noreply.github.com>
2025-09-30 14:05:38 +0530

    Create navigation.js

---
+++ src/data/navigation.js
@@ -0,0 +1,7 @@
+// src/data/navigation.js
+export const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'About', href: '/about', current: false },
+  { name: 'Services', href: '/services', current: false },
+  { name: 'Contact', href: '/contact', current: false },
+];
