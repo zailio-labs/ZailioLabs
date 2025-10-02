e2e103f4c285b886244158e4e7f8332e681129ea
Zailio Labs <100409086+zailio-labs@users.noreply.github.com>
2025-10-02 21:45:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,21 +6,21 @@ const nextConfig = {
   },
   env: {
     // Database
-    MONGODB_URI: process.env.MONGODB_URI || '',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://zailiolabs:zailiolabs@cluster0.bxtjsvf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     
     // Authentication
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || '',
     NEXTAUTH_URL: process.env.NEXTAUTH_URL || '',
     
     // Zoho Mail Configuration
-    ZOHO_EMAIL: process.env.ZOHO_EMAIL,
-    ZOHO_PASSWORD: process.env.ZOHO_PASSWORD,
+    ZOHO_EMAIL: process.env.ZOHO_EMAIL || '',
+    ZOHO_PASSWORD: process.env.ZOHO_PASSWORD || '',
     ZOHO_HOST: process.env.ZOHO_HOST || 'smtp.zoho.com',
     ZOHO_PORT: process.env.ZOHO_PORT || '587',
     
     // Admin Credentials
     ADMIN_EMAIL: process.env.ADMIN_EMAIL || '',
-    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'admin123',
   },
 }
 
